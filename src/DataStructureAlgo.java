91caecd6b4a8899ddfae81275553c93a74daf4d8
its-shreybhatt <shreybhatt2@gmail.com>
2023-03-17 18:29:42 +0530

    Added : Code to sort the String Array using Insertion

--- src/DataStructureAlgo.java
+++ src/DataStructureAlgo.java
@@ -1,17 +1,16 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
-public class Main {
+public class DataStructureAlgo {
     public static void main(String[] args) {
         String word = "abc";
+        DataStructureAlgo object = new DataStructureAlgo();
 
-//        permutation(word);
-//        solve(word.toCharArray(), 0);
 
-        binarySearch();
+//        object.permutation(word);
+//        object.solve(word.toCharArray(), 0);
+//        object.binarySearch();
+        object.insertionSort();
     }
 
-    public static void permutation(String word) {
+    public void permutation(String word) {
         for (int i = 0; i < word.length(); i++) {
             for (int j = 0; j < word.length(); j++) {
                 for (int k = 0; k < word.length(); k++) {
@@ -31,7 +30,7 @@ public class Main {
         arr[idx2] = temp;
     }
 
-    public static void solve(char[] arr, int idx) {
+    public void solve(char[] arr, int idx) {
         if (idx == arr.length - 1) { //Base condition of recursion
             System.out.print(String.valueOf(arr) + " ");
         }
@@ -44,7 +43,7 @@ public class Main {
         }
     }
 
-    public static void binarySearch() {
+    public void binarySearch() {
         String[] array = {"we", "the", "people", "of", "India"};
         stringBinary(array, "people");
     }
@@ -58,11 +57,28 @@ public class Main {
                 min = mid + 1;
             } else if (array[mid].compareTo(search) > 0) {
                 max = mid - 1;
-            } else if (array[mid].compareTo(search)==0){
+            } else if (array[mid].compareTo(search) == 0) {
                 System.out.println("Key found");
                 return;
             }
         }
         System.out.println("key not found ");
+    }
+
+    public void insertionSort() {
+        String[] array = {"shrey", "bhatt", "is", "working", "on", "it"};
+        String max = "null";
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareToIgnoreCase(array[j]) > 0) {
+                    max = array[i];
+                    array[i] = array[j];
+                    array[j] = max;
+                }
+            }
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
